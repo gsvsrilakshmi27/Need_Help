19148de2ab1b136ae573c1d8bf77e29d18006496
deepak3108 <deepakbhimavarapu@gmail.com>
2015-11-25 17:27:12 +0530

    registrations into database

--- need_help/data/urls.py
+++ need_help/data/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^validate/$', views.validate, name='validate'),
+    url(r'^registration/$', views.registration, name = 'registration'),
 ]
