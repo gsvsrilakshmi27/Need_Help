bf2c7fe983ccfcd18ea7427dddbb9ea28226f1a1
deepak3108 <deepakbhimavarapu@gmail.com>
2015-11-30 12:29:25 +0530

    full project except ui

--- need_help/data/urls.py
+++ need_help/data/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
 	url(r'^validate1/$', views.validate1, name='validate1'),
 	url(r'^validate2/$', views.validate2, name='validate2'),
 	url(r'^validate3/$', views.validate3, name='validate3'),
+	url(r'^validate4/$', views.validate4, name='validate4'),
     url(r'^registration/$', views.registration, name = 'registration'),
+    # url(r'^retrieval/$', views.retrieval, name = 'retrieval'),
     url(r'^index/$',views.index,name = 'index'),
 ]
